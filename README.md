8f406208db65ef3bf39097bc62070222d1671aab
Jan Breuer <j.breuer@ostfalia.de>
2023-11-17 13:42:52 +0100

    usb fs as console

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # f7ZephyrOszi
+```bash
+west build -b stm32f746g_disco -p auto
+```
--- src/main.c
+++ src/main.c
@@ -1,12 +1,35 @@
 #include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
+#include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/gpio.h>
 #include "adc.h"
 
 #define SLEEP_TIME_MS  1000
 
 
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+	     "Console device is not ACM CDC UART device");
+
 int main(void)
 {
+	/* ENABLE USB FS AS CONSOLE FOR PRINTK */
+	const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+	if (usb_enable(NULL)) {
+		return 0;
+	}
+
+	/* Poll if the DTR flag was set */
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		/* Give CPU resources to low priority threads. */
+		k_sleep(K_MSEC(100));
+	}
+	printk("USB Initialized\n");
+
 	int ret;
 
 	ret = adc_init();
@@ -18,6 +41,7 @@ int main(void)
 	while (1) {
 		HAL_ADC_Start_IT(&hadc1);
 		k_msleep(SLEEP_TIME_MS);
+		printk("Hi\n");
 	}
 	return 0;
 }
